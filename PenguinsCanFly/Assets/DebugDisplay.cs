66d0655e566edff2ff11560d402edf70d75344ab
arthurliu <theonlyarthurliu@gmail.com>
2022-01-20 21:40:43 -0800

    Working tracking hands handlebar z

--- PenguinsCanFly/Assets/DebugDisplay.cs
+++ PenguinsCanFly/Assets/DebugDisplay.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class DebugDisplay : MonoBehaviour
 {
     private List<string> debugLogs = new List<string>();
+    private Dictionary<string, string> saveValues = new Dictionary<string, string>();
     public Text Display;
+    public int NUM_LOG_ITEMS = 12;
     
     protected void OnEnable()
     {
@@ -23,13 +26,32 @@ public class DebugDisplay : MonoBehaviour
     {
         if (type == LogType.Log)
         {
-            debugLogs.Add(logString);
-
-            string displayText = "";
-            for (int i = debugLogs.Count - 1; i >= 0 && debugLogs.Count - i < 12; i--)
+            if (logString.StartsWith("SAVE:"))
             {
-                displayText = debugLogs[i] + "\n" + displayText;;
+                string[] components = logString.Split(':');
+                if (components.Length != 3)
+                {
+                    debugLogs.Add("ERROR: BAD DEBUG LOG!!!!: " + logString);
+                }
 
+                saveValues[components[1]] = components[2];
+            }
+            else
+            {
+                debugLogs.Add(logString);
+            }
+                        
+            string displayText = "";
+            for (int i = debugLogs.Count - 1; i >= 0 && debugLogs.Count - i < NUM_LOG_ITEMS - saveValues.Count; i--)
+            {
+                displayText = debugLogs[i] + "\n" + displayText;
+
+            }
+
+            displayText += "---\n";
+            foreach (string key in saveValues.Keys)
+            {
+                displayText += key + ": " + saveValues[key] + "\n";
             }
             Display.text = displayText;
         }
--- PenguinsCanFly/Assets/HandlebarHandle.cs
+++ PenguinsCanFly/Assets/HandlebarHandle.cs
@@ -6,7 +6,8 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class HandlebarHandle : XRBaseInteractable
 {
     
-    Transform thingToRotate = null;
+    public Transform thingToRotate = null;
+    public float MAX_ROTATION_DEGREES = 35;
     
     // Start is called before the first frame update
     void Start()
@@ -29,12 +30,56 @@ public class HandlebarHandle : XRBaseInteractable
     
         if (isSelected)
         {
+
+            
+            // thingToRotate.RotateAround(rotationPivot.position, thingToRotate.forward, 1);
+            // thingToRotate.RotateAround(rotationPivot.position, ); 
+            // Quaternion targetRotation = Quaternion.Euler(thingToRotate.rotation.eulerAngles.x,
+            //                                              thingToRotate.rotation.eulerAngles.y,
+            //                                              selectInteractor.transform.rotation.eulerAngles.z);
+            // thingToRotate.rotation = Quaternion.RotateTowards(thingToRotate.rotation, targetRotation, 5);
+            //
+            // if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.Dynamic)
+            // {
+            //     Debug.Log("Degree " + targetRotation.eulerAngles);
+            // }
+
+            Vector3 relativePos = selectInteractor.transform.position - thingToRotate.position;
+            // Vector3 relativePos = thingToRotate.InverseTransformDirection(selectInteractor.transform.position);
+            // Vector3 relativeYPos = new Vector3(relativePos.x, relativePos.y, 0);
+            // Vector3 
+
+            Vector3 projectedVector = Vector3.ProjectOnPlane(relativePos, thingToRotate.forward);
+            float amountToRotateZ = - Vector3.SignedAngle(projectedVector, thingToRotate.right, thingToRotate.forward);
+            // Check that xyAngle is within bounds
+            Vector3 currentAngle = thingToRotate.eulerAngles;
+            if (currentAngle.z + amountToRotateZ <= MAX_ROTATION_DEGREES
+                || currentAngle.z + amountToRotateZ >= 360 - MAX_ROTATION_DEGREES)
+            {
+                thingToRotate.Rotate(new Vector3(0, 0, amountToRotateZ));
+            }
+            // Quaternion targetRotation = Quaternion.FromToRotation(Vector3.right, relativeYPos);
+            
             if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.Dynamic)
             {
-                Debug.Log("Handle bar being held by " + selectInteractor);
+                Debug.Log("SAVE:xyAngle:" + amountToRotateZ);
+                Debug.Log("SAVE:objectRotation:" + thingToRotate.eulerAngles);
+
+                // Debug.Log("Rotation goal: " + targetRotation.eulerAngles + " Relative pos: " + relativePos);
             }
+            // thingToRotate.rotation = Quaternion.Lerp(thingToRotate.rotation, targetRotation, 0.3);
+            // Do some smoothing
+            // thingToRotate.rotation *= targetRotation;
         }
+        
     }
+    
+        protected override void OnSelectEntered(SelectEnterEventArgs args)
+        {
+            base.OnSelectEntered(args);
+            Debug.Log("HandlebarHandle: select entered: " + args.interactorObject);
+            selectInteractor = args.interactorObject;
+        }
 
 
     // Update is called once per frame
