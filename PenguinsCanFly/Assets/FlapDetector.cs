b89adeac3b839e893b98f8d60426dd51ac1d8265
Taylor Ka <taylorka@live.com>
2022-01-23 19:49:13 -0800

    scale num flaps for launch speed

--- PenguinsCanFly/Assets/FlapDetector.cs
+++ PenguinsCanFly/Assets/FlapDetector.cs
@@ -6,21 +6,29 @@ using UnityEngine;
 public class FlapDetector : MonoBehaviour
 {
     public GestureGroup top;
-
     public GestureGroup bottom;
+    
+    public Rigidbody rb;
+    public Transform gliderDirection;
+    
+    private static float FLAP_SPEED_MULTIPLIER = 0.6f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // TODO: do something interesting with this count
         int topCount = top.GetHitCount();
         int bottomCount = bottom.GetHitCount();
         Debug.Log("SAVE:flaps: top-" + topCount + " bottom-" + bottomCount + " min-" + Math.Min(topCount, bottomCount));
+
+        // TODO: if you stop flapping, should your speed increase
+        Vector3 localV = gliderDirection.InverseTransformDirection(rb.velocity);
+        localV.z = Math.Min(topCount, bottomCount) * FLAP_SPEED_MULTIPLIER;
+        rb.velocity = gliderDirection.TransformDirection(localV);
     }
 }
