0ee97ab194f451af05a771a65ee3c8afe90f086a
James Qiu <qiujames@cs.washington.edu>
2022-02-06 16:59:22 -0800

    Fix backwards edge case (transition from front to back isn't clean but should be edge case)

--- PenguinsCanFly/Assets/Scripts/FlipperShoulderScript.cs
+++ PenguinsCanFly/Assets/Scripts/FlipperShoulderScript.cs
@@ -15,6 +15,7 @@ public class FlipperShoulderScript : MonoBehaviour
     public const float shoulderXOffset = 0.09f;
     public const float shoulderYOffset = -0.2f;
     public const float shoulderZOffset = -0.05f;
+    public const float backwardAngleThreshold = 120f;
 
     public const float flipperHandWidth = 0.12f;
 
@@ -29,8 +30,23 @@ public class FlipperShoulderScript : MonoBehaviour
     void Update()
     {
         // Compute rotation from forward for the shoulder offset
-        float angle = Vector2.SignedAngle(new Vector2(gliderDirection.transform.forward.x, gliderDirection.transform.forward.z), new Vector2(0, 1));
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
+        Quaternion rotation;
+        Vector3 gliderForward = gliderDirection.transform.forward;
+
+        float cameraGliderAngle = Vector2.SignedAngle(new Vector2(this.mainCamera.transform.forward.x, mainCamera.transform.forward.z), new Vector2(gliderDirection.transform.forward.x, gliderDirection.transform.forward.z));
+        if (Mathf.Abs(cameraGliderAngle) < backwardAngleThreshold)
+        {
+            float gliderFrontAngle = Vector2.SignedAngle(new Vector2(gliderForward.x, gliderForward.z), new Vector2(0, 1));
+            rotation = Quaternion.AngleAxis(gliderFrontAngle, Vector3.up);
+
+        }
+        else
+        {
+            // If user starts to look backwards, flip their orientation 
+            Vector3 gliderBack = gliderForward * -1;
+            float gliderBackAngle = Vector2.SignedAngle(new Vector2(gliderBack.x, gliderBack.z), new Vector2(0, 1));
+            rotation = Quaternion.AngleAxis(gliderBackAngle, Vector3.up);
+        }
 
         // Set start point of shoulder
         Vector3 shoulderPosition = this.mainCamera.transform.position + (rotation * shoulderOffset);
