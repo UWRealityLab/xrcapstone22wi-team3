eace8c4e0cdef6f900e368709985f8ad237a885a
Taylor Ka <taylorka@live.com>
2022-02-18 12:43:05 -0800

    balloons float up and down

--- PenguinsCanFly/Assets/Scripts/BalloonObstacle.cs
+++ PenguinsCanFly/Assets/Scripts/BalloonObstacle.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class BalloonObstacle : Obstacle
 {
+    private float _speed;
+    private float _height;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _speed = (Random.value < .5 ? 1 : -1) * Random.Range(0.3f, 0.6f);
+        _height = Random.Range(0.03f, 0.05f);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        float y = Mathf.Sin(Time.time * _speed) * _height;
+        transform.position += Vector3.up * y;
     }
     
     public override float GetSpawnOffsetLowerBound()
@@ -23,7 +27,7 @@ public class BalloonObstacle : Obstacle
 
     public override float GetSpawnOffsetUpperBound()
     {
-        return 50f;
+        return 100f;
     }
 
     public override Quaternion GetSpawnRotation()
