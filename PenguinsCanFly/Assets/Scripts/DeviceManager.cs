a514935ef0c3bb14c9544943e102ea72584ad3fb
arthurjl <artliu@cs.washington.edu>
2022-02-19 21:50:54 -0800

    remove pitch from hand angle and use trigger press instead. refactor devices into a singular class

---
+++ PenguinsCanFly/Assets/Scripts/DeviceManager.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+public class DeviceManager : MonoBehaviour
+{
+    
+    public InputDevice rightHandDevice;
+    public InputDevice leftHandDevice;
+
+    private static DeviceManager _instance;
+    public static DeviceManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
+    }
+    
+
+    void Start()
+    {
+        var inputDevices = new List<InputDevice>();
+        InputDevices.GetDevices(inputDevices);
+
+        foreach (var device in inputDevices)
+        {
+            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
+        }
+        
+        // Devices we are actually  using:
+        InputDeviceCharacteristics rightControllerCharacteristics =
+            InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, inputDevices);
+        
+        foreach (var device in inputDevices)
+        {
+            Debug.Log(string.Format("With proper characteristics '{0}' and role '{1}'", device.name, device.role.ToString()));
+        }
+
+        if (inputDevices.Count > 0)
+        {
+            rightHandDevice = inputDevices[0];
+        }
+        
+        InputDeviceCharacteristics leftControllerCharacteristics  =
+            InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(leftControllerCharacteristics, inputDevices);
+        if (inputDevices.Count > 0)
+        {
+            leftHandDevice = inputDevices[0];
+        }
+    }
+}
--- PenguinsCanFly/Assets/Scripts/VoiceSystemActivator.cs
+++ PenguinsCanFly/Assets/Scripts/VoiceSystemActivator.cs
@@ -11,38 +11,10 @@ using Oculus.Voice;
 
 public class VoiceSystemActivator : MonoBehaviour
 {
-    private InputDevice targetDevice;
 
     [SerializeField] private Wit wit;
 
     private bool pressed = false;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        var inputDevices = new List<UnityEngine.XR.InputDevice>();
-        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
-
-        foreach (var device in inputDevices)
-        {
-            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
-        }
-        
-        // Devices we are actually  using:
-        InputDeviceCharacteristics rightControllerCharacteristics =
-            InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
-        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, inputDevices);
-        
-        foreach (var device in inputDevices)
-        {
-            Debug.Log(string.Format("With proper characteristics '{0}' and role '{1}'", device.name, device.role.ToString()));
-        }
-
-        if (inputDevices.Count > 0)
-        {
-            targetDevice = inputDevices[0];
-        }
-    }
 
     // Update is called once per frame
     void Update()
@@ -54,7 +26,7 @@ public class VoiceSystemActivator : MonoBehaviour
         }
         
         Debug.Log("SAVE:wit: " + wit);
-        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+        DeviceManager.Instance.leftHandDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
         if (primaryButtonValue != pressed)
         {
             pressed = primaryButtonValue;
