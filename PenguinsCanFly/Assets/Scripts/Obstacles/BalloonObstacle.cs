51f239c1bd9ca8b192744cad423d5d79753e9c45
Taylor Ka <taylorka@live.com>
2022-02-28 21:50:07 -0800

    remove balloon movement

--- PenguinsCanFly/Assets/Scripts/Obstacles/BalloonObstacle.cs
+++ PenguinsCanFly/Assets/Scripts/Obstacles/BalloonObstacle.cs
@@ -4,24 +4,6 @@ using UnityEngine;
 
 public class BalloonObstacle : Obstacle
 {
-    private float _speed;
-    private float _height;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        _speed = (Random.value < .5 ? 1 : -1) * Random.Range(0.3f, 0.6f);
-        _height = Random.Range(0.04f, 0.07f);
-    }
-
-    // Update is called once per frame
-    new void Update()
-    {
-        base.Update();
-        float y = Mathf.Sin(Time.time * _speed) * _height;
-        transform.position += Vector3.up * y;
-    }
-
     public override bool ShouldSpawnOnGround()
     {
         return false;
