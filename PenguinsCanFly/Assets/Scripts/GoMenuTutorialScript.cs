9f23cafef8f63b8b5dea7a26ffde5ac037a355f6
James Qiu <qiujames@cs.washington.edu>
2022-03-04 21:31:06 -0800

    Add go to tutorial menu script

---
+++ PenguinsCanFly/Assets/Scripts/GoMenuTutorialScript.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GoMenuTutorialScript : XRGrabInteractable
+{
+    public TextMeshProUGUI returnHomeText;
+
+    private bool _goMenuCalled = false;
+    private float _goMenuCallTime = -1f;
+
+    private const float TimeBeforeReset = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_goMenuCalled && isSelected)
+        {
+            // Initiate go menu timer
+            _goMenuCalled = true;
+            _goMenuCallTime = Time.time;
+            Invoke("GoMenu", TimeBeforeReset);
+        }
+        else if (_goMenuCalled)
+        {
+            // Display go home count down
+            int timeLeft = (int)Math.Ceiling(TimeBeforeReset - (Time.time - _goMenuCallTime));
+            returnHomeText.text = "Returning home in " + timeLeft;
+        }
+    }
+
+    private void GoMenu()
+    {
+        TutorialGameController.Instance.ResetToMenu();
+    }
+
+}
